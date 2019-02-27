55991665020dcc91c26a94e4ddf771e87393fce2
TerryShu <wind850101@gmail.com>
2019-02-27 07:30:03 -0800

    func_ptr without branch

--- main.c
+++ main.c
@@ -5,19 +5,17 @@ int plus(int a, int b) { return a + b; }
 int minus(int a, int b) { return a - b; }
 int multiply(int a, int b) { return a * b; }
 int divided(int a, int b) { return a / b; }
+
 int main()
 {
 	int a, c;
 	char b;
 	printf("key a Function \nEX: 1 + 1\n");
 	scanf("%i %c %i", &a, &b, &c);
-	//IF ELSE IF
-	if (b == '+')
-		printf("%d %c %d = %d\n", a, b, c, plus(a, c));
-	else if (b == '-')
-		printf("%d %c %d = %d\n", a, b, c, minus(a, c));
-	else if (b == '*')
-		printf("%d %c %d = %d\n", a, b, c, multiply(a, c));
-	else if (b == '/')
-		printf("%d %c %d = %d\n", a, b, c, divided(a, c));
+	int (*func_ptr[6])(int,int) ;
+	func_ptr[(int)'+'-42] = plus ;
+	func_ptr[(int)'-'-42] = minus ;
+	func_ptr[(int)'*'-42] = multiply ;
+	func_ptr[(int)'/'-42] = divided ; 
+	printf("%d %c %d = %d\n", a, b, c, (*func_ptr[(int)b-42])(a,c)) ;
 }
